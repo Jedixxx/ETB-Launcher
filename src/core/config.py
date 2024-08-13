8d1b03d4b980cfb011414b690ab6f782265bfcaf
Jedixxx <104788360+Jedixxx@users.noreply.github.com>
2024-08-13 03:44:44 +0100

    app_config.json added some config, added constants file, added support for relative paths in config.py

--- src/core/config.py
+++ src/core/config.py
@@ -8,13 +8,19 @@ class Config:
     A class to load and store the config and import file paths for the mod manager
     """
 
-    def __init__(self, config_file: str = 'config/mod_config.json'):
+    def __init__(self, config_path: str = 'config/production_config.json'):
         """
         Initialise Config with a specified configuration file
 
-        :param config_file: Path to the configuration file
+        :param config_path: Absolute or Relative path to the configuration file
         """
-        self.config_file = config_file
+        is_path_absolute = os.path.isabs(config_path)
+        if is_path_absolute:
+            self.config_path = config_path
+        else:
+            content_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+            self.config_path = os.path.join(content_root, config_path)
+
         self.config_data = self._load_config()
 
     def _load_config(self) -> Dict[str, Any]:
@@ -23,16 +29,15 @@ class Config:
 
         :return: A dictionary containing the config data
         """
-        if os.path.exists(self.config_file):
-            try:
-                with open(self.config_file, 'r') as file:
-                    config_data = json.load(file)
-                    print(f"Config loaded from {self.config_file}")
-                    return config_data
-            except json.JSONDecodeError as e:
-                print(f"Error decoding JSON from {self.config_file}: {e}")
-                return {}
-        else:
-            print(f"No configuration file found at {self.config_file}")
+        try:
+            with open(self.config_path, 'r') as file:
+                config_data = json.load(file)
+                print(f"Config loaded from {self.config_path}")
+                return config_data
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON from {self.config_path}: {e}")
+            return {}
+        except FileNotFoundError:
+            print(f"No configuration file found at {self.config_path}")
             return {}
 
